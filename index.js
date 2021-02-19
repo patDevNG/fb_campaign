23fb9474625e9e3f455bf86b4d67e2461dba06f5
Okuns Patrick <cgrleadership@gmail.com>
2021-02-19 22:56:13 +0100

    added image upload capability

--- index.js
+++ index.js
@@ -1,112 +1,169 @@
-const express = require('express');
-const axios = require('axios');
-const server = express()
-const bodyParser = require('body-parser')
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const multer = require('multer')
+const server = express();
+const bodyParser = require("body-parser");
+const Busboy = require("busboy");
 const port = 4000;
-const cors = require('cors')
-server.use(bodyParser.urlencoded({ extended: true })) 
+const cors = require("cors");
+const { log } = require("console");
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(cors());
-server.use(bodyParser.json())
+server.use(bodyParser.json());
 
+server.get("/", async (req, res) => {
+  res.send("Testing my api:8084");
+});
 
-server.get("/", async(req,res)=>{
-    res.send("Testing my api:8084")
-})
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, './files/images/');
+    } else {
+      cb({ message: 'this file is neither a video or image file' }, false);
+    }
+  },
+  filename(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
+server.post("/create_fb_ad_campaign", upload.any(), async (req, res) => {
+  // // console.log(req.body, "this is the request from waitstaff-ui");
+  // console.log(req.files, 'files')
+  console.log(req.body)
+  const base64String = fs.readFileSync(req.files[0].path).toString("base64");
+ 
+  // console.log({base64String})
 
-server.post('/create_fb_ad_campaign', async(req,res)=>{
-    console.log(req.body, "this is the request from waitstaff-ui")
-    let {adset_name,age_range,gender,budget,ad_desc} = req.body
+  let {adset_name,age_range,gender,budget,ad_desc,location_targeting_people,device_platforms,platform_facebook,platform_messenger,
+    budget_type,start_time,end_time,ad_title,ad_text, merchant_id, call_to_action
+  } = req.body
+  
   //     let budget = req.params.budget;
    budget = parseInt(budget)*100;
-  //    console.log(budget,"budgetttttttttttttt");
+     console.log(budget,"budgetttttttttttttt");
   //     const promoId = req.params.promo_id;
-  
+
   //     const promoToBoost = await Promo.findById(promoId)
   //  console.log(promoToBoost);
   //  const{promo_text,promo_description,image_url,promo_name, merhant_id,location_id,promo_type} = promoToBoost;
-    const bizSdk = require('facebook-nodejs-business-sdk');
+  const bizSdk = require('facebook-nodejs-business-sdk');
+
+  const accessToken = 'EAADxqKWGDYkBAO96f8ZCeZBZAnyCZBMvJFHUuLqAqZCRQxZAOHzxdDZCqwGrGRQ159MlbkwZAkj0jFhCL2uXdm6enEL3Qfmh0eJLIutl5Tb40qHQP6CZBOHpNdY4p3LiZCJmLxdtBKNpX3O4T2tUg3FD6F4ZBcKQ4UEzfaFGZCa5WePZAQgZDZD';
+  // const accountId = 'act_2517267978507637';
+  const accountId ='act_210261016055454'
+
+  const FacebookAdsApi = bizSdk.FacebookAdsApi.init(accessToken);
+  const AdAccount = bizSdk.AdAccount;
+  const Campaign = bizSdk.Campaign;
+  const account = new AdAccount(accountId);
+  console.log(account.id, "testing ")
+  try {
+      let special_ad_category = "NONE"
+      const newCampaign = await account.createCampaign(
+          [],
+          {
+              special_ad_categories :[],
+              [Campaign.Fields.name]: adset_name, // Each object contains a fields map with a list of fields supported on that object.
+              [Campaign.Fields.status]: Campaign.Status.paused,
+              [Campaign.Fields.objective]: 'MESSAGES',
+            }
+      )
+      const {id} =newCampaign ;
+      console.log(id, "Campaign Id");
+      let fields, params;
+  fields = [
+  ];
+  params = {
+    'name' : adset_name,
+    'daily_budget' : budget,
+    'campaign_id' : id,
+    'bid_amount' : budget,
+    'billing_event' : 'IMPRESSIONS',
+    'optimization_goal' : 'CONVERSATIONS',
+    'targeting' : {'age_min':age_range.age_min,'age_max':age_range.age_max,'behaviors':[],'genders':[gender],'geo_locations':{'countries':['US'],}},
+    'status' : 'PAUSED',
+    "device_platforms ": [
+      "all "
+    ]
+  };
+  const adsets =await account.createAdSet(
+      fields,
+      params
+    );
+    console.log(adsets.id, "adset Id")
+
+    const adImage = await account.createAdImage([], {
+      bytes: base64String,
+    });
+
+    fs.unlinkSync(req.files[0].path);
+    const adCreative = await account.createAdCreative(
+        [],
+        {
+          'name':adset_name,
+          "object_story_spec":{
+              "link_data": {
+                "call_to_action": {"type":"SHOP_NOW","value":{"app_destination":"MESSENGER"}},
+                "image_hash": adImage.images.bytes.hash,
+                "link": "www.joyup.me",
+                "message": ad_desc,
+              },
+              "page_id": "270763243298231"
+            }
+
+        }
+    )
+    console.log(adCreative.id, "Ad creative ID")
+
+    const createAd = await account.createAd(
+        [],
+        {
+          'name' : ad_title,
+          'adset_id' : adsets.id,
+          'creative' : {'creative_id':adCreative.id},
+          'status' : 'PAUSED',
+        }
+    )
+
+  res.status(200).send(createAd);
+  } catch (error) {
+      console.log(error);
+      res.status(500).send(error)
+  }
+});
+
+server.post("/ad_image", async (req, res) => {
   
-    const accessToken = 'EAADxqKWGDYkBAO96f8ZCeZBZAnyCZBMvJFHUuLqAqZCRQxZAOHzxdDZCqwGrGRQ159MlbkwZAkj0jFhCL2uXdm6enEL3Qfmh0eJLIutl5Tb40qHQP6CZBOHpNdY4p3LiZCJmLxdtBKNpX3O4T2tUg3FD6F4ZBcKQ4UEzfaFGZCa5WePZAQgZDZD';
-    // const accountId = 'act_2517267978507637';
-    const accountId ='act_210261016055454'
-    
-    const FacebookAdsApi = bizSdk.FacebookAdsApi.init(accessToken);
-    const AdAccount = bizSdk.AdAccount;
-    const Campaign = bizSdk.Campaign;
-    const account = new AdAccount(accountId);
-    console.log(account.id, "testing ")
-    try {
-        let special_ad_category = "NONE"
-        const newCampaign = await account.createCampaign(
-            [],
-            {
-                special_ad_categories :[],
-                [Campaign.Fields.name]: adset_name, // Each object contains a fields map with a list of fields supported on that object.
-                [Campaign.Fields.status]: Campaign.Status.paused,
-                [Campaign.Fields.objective]: 'MESSAGES',
-              }
-        )
-        const {id} =newCampaign ;
-        console.log(id, "Campaign Id");
-        let fields, params;
-    fields = [
-    ];
-    params = {
-      'name' : adset_name,
-      'daily_budget' : budget,
-      'campaign_id' : id,
-      'bid_amount' : budget,
-      'billing_event' : 'IMPRESSIONS',
-      'optimization_goal' : 'IMPRESSIONS',
-      'targeting' : {'age_min':age_range.age_min,'age_max':age_range.age_max,'behaviors':[],'genders':[gender],'geo_locations':{'countries':['US'],}},
-      'status' : 'PAUSED',
-      "device_platforms ": [ 
-        "all "
-      ]
-    };
-    const adsets =await account.createAdSet(
-        fields,
-        params
-      );
-      console.log(adsets.id, "adset Id")
-    
-      const adCreative = await account.createAdCreative(
-          [],
-          {
-            'name':adset_name,
-            "object_story_spec":{ 
-                "link_data": { 
-                  "call_to_action": {"type":"ORDER_NOW","value":{"app_destination":"MESSENGER"}}, 
-                  "image_hash": "03b0c12161e5a689354c2d34419d98cc", 
-                  "link": "www.joyup.me", 
-                  "message": ad_desc, 
-                }, 
-                "page_id": "270763243298231" 
-              }
-             
-          }
-      )
-      console.log(adCreative.id, "Ad creative ID")
-    
-      const createAd = await account.createAd(
-          [],
-          {
-            'name' : 'My Ad',
-            'adset_id' : adsets.id,
-            'creative' : {'creative_id':adCreative.id},
-            'status' : 'PAUSED',
-          }
-      )
-      
-       
-    
-        res.status(200).send(createAd)
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
-    }
-  })
-server.listen(process.env.PORT || 8585,()=>{
-    console.log(`Server started at port :8585`)
-})
+  
+  const bizSdk = require("facebook-nodejs-business-sdk");
+
+  const accessToken =
+    "EAADxqKWGDYkBAO96f8ZCeZBZAnyCZBMvJFHUuLqAqZCRQxZAOHzxdDZCqwGrGRQ159MlbkwZAkj0jFhCL2uXdm6enEL3Qfmh0eJLIutl5Tb40qHQP6CZBOHpNdY4p3LiZCJmLxdtBKNpX3O4T2tUg3FD6F4ZBcKQ4UEzfaFGZCa5WePZAQgZDZD";
+  // const accountId = 'act_2517267978507637';
+  const accountId = "act_210261016055454";
+
+  const FacebookAdsApi = bizSdk.FacebookAdsApi.init(accessToken);
+  const AdAccount = bizSdk.AdAccount;
+  const Campaign = bizSdk.Campaign;
+  const account = new AdAccount(accountId);
+
+  let filepath = "image.png";
+  let content = fs.readFileSync(filepath).toString("base64");
+  try {
+    const adImage = await account.createAdImage([], {
+      bytes: content,
+    });
+    res.status(200).send(adImage.images.bytes.hash);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+server.listen(process.env.PORT || 8584, () => {
+  console.log(`Server started at port :8584`);
+});
