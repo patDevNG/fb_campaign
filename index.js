383101f114486583269302c9cf2abf688d61237a
Okuns Patrick <cgrleadership@gmail.com>
2021-03-02 23:48:35 +0100

    chenged access token

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ server.post("/create_fb_ad_campaign", upload.any(), async (req, res) => {
   //  const{promo_text,promo_description,image_url,promo_name, merhant_id,location_id,promo_type} = promoToBoost;
   const bizSdk = require('facebook-nodejs-business-sdk');
 
-  const accessToken = 'EAADxqKWGDYkBAJK2TnmpynTtqXxmYNO53CaMTFmGkrAoOgerkdDlPseZCrJoFYoZAnCRKrHF6iDDFsk4EUcj2vRB3AkuKj23qrBPfMukbIicSzzcw09rP4noe6JIoAwBzPk7C0oo9tnjVtJqgN86ZCm2ymUsN84lWpnAEuDgQZDZD'
+  const accessToken = 'EAADxqKWGDYkBAOyKycXRMMocP77e8lntCZBSeGvK9ZBTSXnXjOLvDQCSm3W7LidEAK9kvFdUm4zDRp1ZAFw3enCnGELHrvlpAVv9eaVAZCZBHDLQve897ZBCl0cDcAD2cTHXDnWDeq3wZBKeTMZAkoxDsXw1VszoVv00Ke4dzZByDlgZDZD'
   // 'EAADxqKWGDYkBAMSNCs1TeIfOL476U7vayOcFUirtey7AAosCJ1ZBPbHJrocyov3isVh5rAfZCUWJhVnZBdtOro1sQnxpZCDGVAjPj0uSIKIXRXTnUuHYeuBRNkLXH1hCqiMRFKNokW8qxIsXVwYiE9EcBy53pv4mb9XO0PdYmZB0ISpNTI3jZCHpBVQk1D3wJYQH5VrQoLqxJv6GKMZBMKHpZCBwFyZAy4Da3r2OFeW9VprEahwkiQJlC';
   // EAADxqKWGDYkBAJK2TnmpynTtqXxmYNO53CaMTFmGkrAoOgerkdDlPseZCrJoFYoZAnCRKrHF6iDDFsk4EUcj2vRB3AkuKj23qrBPfMukbIicSzzcw09rP4noe6JIoAwBzPk7C0oo9tnjVtJqgN86ZCm2ymUsN84lWpnAEuDgQZDZD
   // const accountId = 'act_2517267978507637';
